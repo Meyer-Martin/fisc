0f8ecfcba6ed51a4e619fab10210f1cb7272c2ef
martin <martin.meyer@ynov.com>
2023-02-15 19:20:12 +0100

    retrait des warnings

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from "react";
 import {Login} from "./Login/Login";
-import {BrowserRouter, Route, Routes, useNavigate, redirect} from "react-router-dom";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 import {Dashboard} from "./Dashboard/Dashboard";
 import {Account} from "./Account/Account";
-import {getAuth} from "firebase/auth";
 
 function App() {
-    const auth = getAuth();
-    const user = auth.currentUser;
-
-
-
-
- /*   if (user) {
         return (
             <BrowserRouter>
                 <Routes>
@@ -22,17 +14,5 @@ function App() {
                 </Routes>
             </BrowserRouter>
         )
-    } else {
-
-        return <redirect to="/" />
-
-        // <RedirectToLogin2 />
-    }
 }
-*/
-function RedirectToLogin() {
-    const navigate = useNavigate();
-    navigate('/');
-}
-
 export default App;
