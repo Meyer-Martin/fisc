999767283d83c74cb49e39a7af42a561fbaff590
Martin <martin.meyer@ynov.com>
2023-02-23 12:06:24 +0100

    modification: email + password + nom d'affichage fonctionnel

--- src/Account/Account.js
+++ src/Account/Account.js
@@ -1,47 +1,109 @@
 import React, { useEffect, useState } from "react";
-import { getAuth } from "firebase/auth";
+import { getAuth, updateProfile, updateEmail, updatePassword } from "firebase/auth";
 import { Navbar } from "../Navbar/Navbar";
-import "./Account.css"
+import "./Account.css";
 
 function Account() {
-  const [userData, setUserData] = useState({});
+    const [userData, setUserData] = useState({});
+    const [displayName, setDisplayName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const auth = getAuth();
-    const user = auth.currentUser;
-    if (user !== null) {
-      setUserData({
-        displayName: user.displayName,
-        email: user.email,
-        photoURL: user.photoURL,
-        emailVerified: user.emailVerified,
-        uid: user.uid,
-      });
-    }
-  }, []);
+    useEffect(() => {
+        const auth = getAuth();
+        const user = auth.currentUser;
+        if (user !== null) {
+            setUserData({
+                displayName: user.displayName,
+                email: user.email,
+                photoURL: user.photoURL,
+                emailVerified: user.emailVerified,
+                uid: user.uid,
+            });
+            setDisplayName(user.displayName);
+            setEmail(user.email);
+        }
+    }, []);
 
-  return (
-    <div>
-      <Navbar />
-      <div className="account-page">
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        const auth = getAuth();
+        const user = auth.currentUser;
+        let promises = [];
 
-        {userData.uid !== undefined && (
-          <>
-            <div className="user-info">
-              <div className="displayName"> Nom d'affichage: {userData.displayName}</div>
-              <div className="userData">Email: {userData.email}</div>
-              <div className="emailVerified">Email vérifié: {userData.emailVerified ? "Oui" : "Non"}</div>
+        if (displayName !== userData.displayName) {
+            promises.push(updateProfile(user, { displayName }));
+        }
+
+        if (email !== userData.email) {
+            promises.push(updateEmail(user, email));
+        }
+
+        if (password !== "") {
+            promises.push(updatePassword(user, password));
+        }
+
+        Promise.all(promises)
+            .then(() => {
+                setError(null);
+                alert("Modifications enregistrées avec succès !");
+            })
+            .catch((error) => {
+                setError(error.message);
+            });
+    };
+
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+
+        switch (name) {
+            case "displayName":
+                setDisplayName(value);
+                break;
+            case "email":
+                setEmail(value);
+                break;
+            case "password":
+                setPassword(value);
+                break;
+            default:
+                break;
+        }
+    };
+
+    return (
+        <div>
+            <Navbar />
+            <div className="account-page">
+                {userData.uid !== undefined && (
+                    <>
+                        <div className="user-info">
+                            <div className="displayName"> Nom d'affichage: {userData.displayName}</div>
+                            <div className="userData">Email: {userData.email}</div>
+                            <div className="emailVerified">Email vérifié: {userData.emailVerified ? "Oui" : "Non"}</div>
+                        </div>
+                        <form onSubmit={handleSubmit}>
+                            <div className="form-group">
+                                <label htmlFor="displayName">Nom d'affichage:</label>
+                                <input type="text" id="displayName" name="displayName" value={displayName} onChange={handleChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="email">Email:</label>
+                                <input type="email" id="email" name="email" value={email} onChange={handleChange} />
+                            </div>
+                            <div className="form-group">
+                                <label htmlFor="password">Mot de passe:</label>
+                                <input type="password" id="password" name="password" value={password} onChange={handleChange} />
+                            </div>
+                            <button type="submit">Enregistrer les modifications</button>
+                            {error && <div className="error">{error}</div>}
+                        </form>
+                    </>
+                )}
             </div>
-            <div className="user-actions">
-              <button>Mettre à jour le nom d'affichage</button>
-              <button>Mettre à jour l'email</button>
-              <button>Mettre à jour le mot de passe</button>
-            </div>
-          </>
-        )}
-      </div>
-    </div>
-  );
+        </div>
+    );
 }
 
 export { Account };
