0b3bb11ba36d0ad99bb83eb9c3e93a30680a4337
martin <martin.meyer@ynov.com>
2023-09-16 17:42:06 +0200

    last change

--- src/Login/Login.js
+++ src/Login/Login.js
@@ -7,10 +7,10 @@ import {url} from "../environment";
 import {displayErrorMessage} from "../utils/swal";
 
 function Login() {
-    const [name, setName] = useState("azdazdadd");
-    const [forename, setForename] = useState("azdazdazd");
-    const [email, setEmail] = useState("azdazdazd@gmail.com");
-    const [password, setPassword] = useState("azdazdzad");
+    const [name, setName] = useState("");
+    const [forename, setForename] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
 
     const navigate = useNavigate();
 
